c1f15653425c07c13f910b7cd7ec74c277b814fd
Azizbek Jonibekov <Azizbek_Jonibekov@epam.com>
2021-12-20 21:56:38 +0500

    Initial Commit

---
+++ FilmFest/FilmFestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmFestApp.swift
+//  FilmFest
+//
+//  Created by Azizbek Jonibekov on 20/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FilmFestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
